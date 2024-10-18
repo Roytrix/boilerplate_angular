8563876b7413caf9170ffa0eb199e46b49b638a1
roytrix <roytrix@hotmail.com>
2024-10-18 11:02:21 -0400

    add Basic sidenav with home and about

--- boilerplate_angular/src/app/navigation/navigation.component.ts
+++ boilerplate_angular/src/app/navigation/navigation.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-
 }
